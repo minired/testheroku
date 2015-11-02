b5d2afff8766fe461cfe66a6355f6a90320e4b59
seokjoong <happychrono@gmail.com>
2015-11-03 06:05:09 +0900

    경로수정

--- cmd/go-getting-started/main.go
+++ cmd/go-getting-started/main.go
@@ -24,8 +24,8 @@ func main() {
 
 	router := gin.New()
 	router.Use(gin.Logger())
-	router.LoadHTMLGlob("../../templates/*.tmpl.html")
-	router.Static("../../static", "static")
+	router.LoadHTMLGlob("templates/*.tmpl.html")
+	router.Static("static", "static")
 
 	router.GET("/test", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
