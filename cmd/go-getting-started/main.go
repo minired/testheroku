e0f3d7035aebe21957a4962f5c3da47fc294b59a
LukeKim <happychrono@gmail.com>
2015-11-02 17:13:29 +0900

    test route

--- cmd/go-getting-started/main.go
+++ cmd/go-getting-started/main.go
@@ -20,9 +20,14 @@ func main() {
 	router.LoadHTMLGlob("templates/*.tmpl.html")
 	router.Static("/static", "static")
 
+	router.GET("/test", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	})
+
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
 	})
+	
 
 	router.Run(":" + port)
 }
